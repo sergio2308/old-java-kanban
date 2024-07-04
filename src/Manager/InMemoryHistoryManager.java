ae1f07d49cc9a2b869e51425e232152598176294
sergio2308 <serg2308n@yandex.ru>
2024-07-04 23:54:48 +0300

    feature: реализовать метод remove

--- src/Manager/InMemoryHistoryManager.java
+++ src/Manager/InMemoryHistoryManager.java
@@ -34,13 +34,39 @@ public class InMemoryHistoryManager implements  HistoryManager{
         history.put(task.getId(), newNode);
     }
 
+    public List<Task> getTasks() {
+        List<Task> tasks = new ArrayList<>();
+        Node<Task> currentNode = head;
+        while (currentNode != null) {
+            tasks.add(currentNode.task);
+            currentNode = currentNode.next;
+        }
+        return tasks;
+    }
+
     @Override
     public void remove(int id) {
-
+        Node nodeToRemove = history.remove(id);
+        if (nodeToRemove == null) {
+            return;
+        }
+        if (nodeToRemove.prev == null && nodeToRemove.next == null) {
+            head = null;
+            tail = null;
+        } else if (nodeToRemove.prev == null) {
+            head = nodeToRemove.next;
+            nodeToRemove.next.prev = null;
+        } else if (nodeToRemove.next == null) {
+            tail = nodeToRemove.prev;
+            nodeToRemove.prev.next = null;
+        } else {
+            nodeToRemove.next.prev = nodeToRemove.prev;
+            nodeToRemove.prev.next = nodeToRemove.next;
+        }
     }
 
     @Override
     public List<Task> getHistory() {
-        return List.copyOf(history);
+        return getTasks();
     }
 }
