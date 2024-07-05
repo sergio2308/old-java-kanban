5ac7e0be5118669cf6262b375e1289d41eaf93c7
sergio2308 <serg2308n@yandex.ru>
2024-07-05 16:47:22 +0300

    test: написать тесты методов remove и addHistory

--- src/test/InMemoryHistoryManagerTest.java
+++ src/test/InMemoryHistoryManagerTest.java
@@ -17,4 +17,21 @@ class InMemoryHistoryManagerTest {
             assertEquals(task1, task, "Задачи не сохраняются в историю!");
         }
     }
+
+    void shouldRemoveTasks() {
+        HistoryManager historyManager = Managers.getDefaultHistoryManager();
+        Task task1 = new Task("Задача1", "Описание1", Status.NEW);
+        Task task2 = new Task("Задача2", "Описание2", Status.NEW);
+        Task task3 = new Task("Задача3", "Описание3", Status.NEW);
+        historyManager.addHistory(task1);
+        historyManager.addHistory(task2);
+        historyManager.addHistory(task3);
+        assertEquals(3, historyManager.getHistory().size());
+        historyManager.remove(task1.getId());
+        assertEquals(2, historyManager.getHistory().size());
+        historyManager.remove(task2.getId());
+        assertEquals(1, historyManager.getHistory().size());
+        historyManager.remove(task3.getId());
+        assertTrue(historyManager.getHistory().isEmpty());
+    }
 }
